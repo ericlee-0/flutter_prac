1bd8aa099a5c0b9f169061ab2610a37ebb69ffc4
georgelee-real <mimirprime@gmail.com>
2020-10-12 22:26:48 -0400

    adding provider to manage rebuilding widgets

--- shop_app/lib/main.dart
+++ shop_app/lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
-
+import 'package:provider/provider.dart';
 
 import './screens/products_overview_screen.dart';
+import './screens/product_detail_screen.dart';
+
+import 'providers/products.dart';
+
 void main() {
   runApp(MyApp());
 }
@@ -10,12 +14,20 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'MyShop',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return ChangeNotifierProvider(
+      create: (ctx)=> Products(),
+      child: MaterialApp(
+        title: 'MyShop',
+        theme: ThemeData(
+          primarySwatch: Colors.purple,
+          accentColor: Colors.deepOrange,
+          fontFamily: 'Lato',
+        ),
+        home: ProductOverviewScreen(),
+        routes: {
+          ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
+        },
       ),
-      home: ProductOverviewScreen(),
     );
   }
 }
