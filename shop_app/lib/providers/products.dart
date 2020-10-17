d5192e1e1581279f06393c15db8f38c15818ed0f
georgelee-real <mimirprime@gmail.com>
2020-10-17 12:50:46 -0400

    delete with throwing exception

--- shop_app/lib/providers/products.dart
+++ shop_app/lib/providers/products.dart
@@ -3,6 +3,7 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import './product.dart';
+import '../models/http_exception.dart';
 
 class Products with ChangeNotifier {
   List<Product> _items = [
@@ -131,7 +132,7 @@ class Products with ChangeNotifier {
   Future<void> updateProduct(String id, Product newProduct) async {
     final prodIndex = _items.indexWhere((element) => element.id == id);
     // const String _id = id;
-    print(id);
+
     if (prodIndex >= 0) {
       final url =
           'https://flutter-shop-app-7d1f3.firebaseio.com/products/$id.json';
@@ -149,8 +150,21 @@ class Products with ChangeNotifier {
     }
   }
 
-  void deleteProduct(String id) {
-    _items.removeWhere((element) => element.id == id);
+  Future<void> deleteProduct(String id) async {
+    final url = 'https://flutter-shop-app-7d1f3.firebaseio.com/products/$id';
+    final existingProductIndex =
+        _items.indexWhere((element) => element.id == id);
+    var existingProduct = _items[existingProductIndex];
+    _items.removeAt(existingProductIndex);
     notifyListeners();
+    final response = await http.delete(url);
+
+    if (response.statusCode >= 400) {
+      _items.insert(existingProductIndex, existingProduct);
+      notifyListeners();
+      throw HttpException('Could not delete product!');
+    }
+
+    existingProduct = null;
   }
 }
--- shop_app/lib/widgets/user_product_item.dart
+++ shop_app/lib/widgets/user_product_item.dart
@@ -4,6 +4,7 @@ import 'package:provider/provider.dart';
 
 import '../providers/products.dart';
 import '../screens/edit_product_screen.dart';
+
 class UserProductItem extends StatelessWidget {
   final String id;
   final String title;
@@ -12,6 +13,7 @@ class UserProductItem extends StatelessWidget {
   UserProductItem(this.id, this.title, this.imageUrl);
   @override
   Widget build(BuildContext context) {
+    final scaffold = Scaffold.of(context);
     return ListTile(
       title: Text(title),
       leading: CircleAvatar(
@@ -23,14 +25,24 @@ class UserProductItem extends StatelessWidget {
           IconButton(
             icon: Icon(Icons.edit),
             onPressed: () {
-              Navigator.of(context).pushNamed(EditProductScreen.routeName, arguments: id);
+              Navigator.of(context)
+                  .pushNamed(EditProductScreen.routeName, arguments: id);
             },
             color: Theme.of(context).primaryColor,
           ),
           IconButton(
             icon: Icon(Icons.delete),
-            onPressed: () {
-              Provider.of<Products>(context, listen: false).deleteProduct(id);
+            onPressed: () async {
+              try {
+                await Provider.of<Products>(context, listen: false).deleteProduct(id);
+              } catch (error) {
+                scaffold.showSnackBar(SnackBar(
+                  content: Text(
+                    error.toString(),
+                    textAlign: TextAlign.center,
+                  ),
+                ));
+              }
             },
             color: Theme.of(context).errorColor,
           )
