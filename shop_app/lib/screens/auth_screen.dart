6a7f305fc580fa00b2fac8367b553aff31bb8081
georgelee-real <mimirprime@gmail.com>
2020-10-19 19:26:07 -0400

    using animationbuilder more efficiency instead of manually build wit setstate

--- shop_app/lib/screens/auth_screen.dart
+++ shop_app/lib/screens/auth_screen.dart
@@ -129,13 +129,13 @@ class _AuthCardState extends State<AuthCard>
         curve: Curves.fastOutSlowIn,
       ),
     );
-    _heightAnimation.addListener(() {//setup listener
-      setState(() {});
-    });
+    // _heightAnimation.addListener(() {//setup listener
+    //   setState(() {});
+    // });
   }
+
   @override
   void dispose() {
-    
     super.dispose();
     _controller.dispose();
   }
@@ -234,15 +234,19 @@ class _AuthCardState extends State<AuthCard>
         borderRadius: BorderRadius.circular(10.0),
       ),
       elevation: 8.0,
-      child: Container(
-        // height: _authMode == AuthMode.Signup ? 320 : 260,
-        height: _heightAnimation.value.height,
-        constraints:
-            // BoxConstraints(minHeight: _authMode == AuthMode.Signup ? 320 : 260),
-            BoxConstraints(minHeight: _heightAnimation.value.height),
-        width: deviceSize.width * 0.75,
-        padding: EdgeInsets.all(16.0),
+      child: AnimatedBuilder(
+        animation: _heightAnimation,
+        builder: (ctx, ch) => Container(
+            // height: _authMode == AuthMode.Signup ? 320 : 260,
+            height: _heightAnimation.value.height,
+            constraints:
+                // BoxConstraints(minHeight: _authMode == AuthMode.Signup ? 320 : 260),
+                BoxConstraints(minHeight: _heightAnimation.value.height),
+            width: deviceSize.width * 0.75,
+            padding: EdgeInsets.all(16.0),
+            child: ch),
         child: Form(
+          //this will pass to builder as ch and won't rerbuild
           key: _formKey,
           child: SingleChildScrollView(
             child: Column(
