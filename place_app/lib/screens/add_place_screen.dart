4ae08a9369bcd7a63d438b34a0a01b078d3c8f91
georgelee-real <mimirprime@gmail.com>
2020-10-20 23:22:48 -0400

    take a picture with camera and store in the device by using path and path-provider

--- place_app/lib/screens/add_place_screen.dart
+++ place_app/lib/screens/add_place_screen.dart
@@ -1,4 +1,10 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
+
+import 'package:provider/provider.dart';
+
+import '../providers/great_places.dart';
+
 
 import '../widgets/image_input.dart';
 
@@ -9,7 +15,21 @@ class AddPlaceScreen extends StatefulWidget {
 }
 
 class _AddPlaceScreenState extends State<AddPlaceScreen> {
-final _titleController = TextEditingController();
+  final _titleController = TextEditingController();
+  File _pickedImage;
+
+  void _selectImage(File pickedImage) {
+    _pickedImage = pickedImage;
+  }
+
+  void _savePlace() {
+    if (_titleController.text.isEmpty || _pickedImage == null) {
+      return;
+    }
+    Provider.of<GreatPlaces>(context, listen: false)
+        .addPlace(_titleController.text, _pickedImage);
+        Navigator.of(context).pop();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -34,15 +54,17 @@ final _titleController = TextEditingController();
                         controller: _titleController,
                         //need to add validation later
                       ),
-                      SizedBox(height: 10,),
-                      ImageInput(),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      ImageInput(_selectImage),
                     ],
                   ),
                 ),
               ),
             ),
             RaisedButton.icon(
-              onPressed: () {},
+              onPressed: _savePlace,
               icon: Icon(Icons.add),
               label: Text('Add Place'),
               elevation: 0,
