85cf4ad5a89481d0287f7750fdccb9ea0547707e
georgelee-real <mimirprime@gmail.com>
2020-10-22 13:41:20 -0400

    adding firebase cloud database and fetch data

--- chat_app/lib/screens/chat_screen.dart
+++ chat_app/lib/screens/chat_screen.dart
@@ -1,16 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class ChatScreen extends StatelessWidget {
+  Future<void> _fireInit() async {
+    WidgetsFlutterBinding.ensureInitialized();
+    await Firebase.initializeApp();
+    FirebaseFirestore.instance
+        .collection('chats/vr7grvLiKtVIsPWvUsYB/messages')
+        .snapshots()
+        .listen((event) {
+          event.docs.forEach((element) { 
+            print(element['text']);
+          });
+      // print(event.docs[0]['text']);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
-          body: ListView.builder(
+      body: ListView.builder(
+        itemCount: 10,
         itemBuilder: (ctx, builder) => Container(
           padding: EdgeInsets.all(8),
           child: Text('This works!'),
         ),
       ),
+      floatingActionButton:
+          FloatingActionButton(child: Icon(Icons.add), onPressed: _fireInit),
     );
   }
 }
