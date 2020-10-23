99994079630882393ae5a54485fc482162a466f1
georgelee-real <mimirprime@gmail.com>
2020-10-23 17:45:03 -0400

    adding username on chat message

--- chat_app/lib/widgets/chat/message_bubble.dart
+++ chat_app/lib/widgets/chat/message_bubble.dart
@@ -1,15 +1,18 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class MessageBubble extends StatelessWidget {
-  MessageBubble(this.message, this.isMe);
+  MessageBubble(this.message, this.userName, this.isMe, {this.key});
 
+  final Key key;
   final message;
+  final String userName;
   final isMe;
 
   @override
   Widget build(BuildContext context) {
     return Row(
-      mainAxisAlignment: isMe? MainAxisAlignment.end: MainAxisAlignment.start,
+      mainAxisAlignment: isMe ? MainAxisAlignment.end : MainAxisAlignment.start,
       children: [
         Container(
           decoration: BoxDecoration(
@@ -27,13 +30,29 @@ class MessageBubble extends StatelessWidget {
             horizontal: 16,
           ),
           margin: EdgeInsets.symmetric(vertical: 4, horizontal: 8),
-          child: Text(
-            message,
-            style: TextStyle(
-              color: isMe
-                  ? Colors.white
-                  : Theme.of(context).accentTextTheme.headline1.color,
-            ),
+          child: Column(
+            crossAxisAlignment:
+                isMe ? CrossAxisAlignment.end : CrossAxisAlignment.start,
+            children: [
+              Text(
+                userName,
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: isMe
+                      ? Colors.white
+                      : Theme.of(context).accentTextTheme.headline1.color,
+                ),
+              ),
+              Text(
+                message,
+                style: TextStyle(
+                  color: isMe
+                      ? Colors.white
+                      : Theme.of(context).accentTextTheme.headline1.color,
+                ),
+                textAlign: isMe ? TextAlign.end : TextAlign.start,
+              ),
+            ],
           ),
         ),
       ],
