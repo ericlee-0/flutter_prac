9b4da0ba307af7886cc7d28fda58971245fabb96
georgelee-real <mimirprime@gmail.com>
2020-10-23 11:52:56 -0400

    adding message bubble

--- chat_app/lib/screens/chat_screen.dart
+++ chat_app/lib/screens/chat_screen.dart
@@ -1,7 +1,11 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 // import 'package:firebase_core/firebase_core.dart';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
+
+import '../widgets/chat/messages.dart';
+import '../widgets/chat/new_message.dart';
 
 class ChatScreen extends StatelessWidget {
   @override
@@ -19,50 +23,64 @@ class ChatScreen extends StatelessWidget {
               items: [
                 DropdownMenuItem(
                   child: Container(
-                    child: Row(children: [
-                      Icon(Icons.exit_to_app),
-                      SizedBox(width: 8,),
-                      Text('Logout')
-                    ],),
+                    child: Row(
+                      children: [
+                        Icon(Icons.exit_to_app),
+                        SizedBox(
+                          width: 8,
+                        ),
+                        Text('Logout')
+                      ],
+                    ),
                   ),
                   value: 'logout',
                 ),
               ],
-              onChanged: (itemIdentifier){
-                if(itemIdentifier == 'logout'){
+              onChanged: (itemIdentifier) {
+                if (itemIdentifier == 'logout') {
                   FirebaseAuth.instance.signOut();
                 }
               })
         ],
       ),
-      body: StreamBuilder(
-        stream: FirebaseFirestore.instance
-            .collection('chats/vr7grvLiKtVIsPWvUsYB/messages')
-            .snapshots(),
-        builder: (ctx, streamSnapshot) {
-          if (streamSnapshot.connectionState == ConnectionState.waiting) {
-            return Center(
-              child: CircularProgressIndicator(),
-            );
-          }
-          final documents = streamSnapshot.data.docs;
-          return ListView.builder(
-            itemCount: documents.length,
-            itemBuilder: (ctx, index) => Container(
-              padding: EdgeInsets.all(8),
-              child: Text(documents[index]['text']),
+      body: Container(
+        child: Column(
+          children: [
+            Expanded(
+              child: Messages(),
             ),
-          );
-        },
+            NewMessage(),
+          ],
+        ),
       ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-        onPressed: () {
-          FirebaseFirestore.instance
-              .collection('chats/vr7grvLiKtVIsPWvUsYB/messages')
-              .add({'text': 'This was added by clicking the button'});
-        },
-      ),
+      // body: StreamBuilder(
+      //   stream: FirebaseFirestore.instance
+      //       .collection('chats/vr7grvLiKtVIsPWvUsYB/messages')
+      //       .snapshots(),
+      //   builder: (ctx, streamSnapshot) {
+      //     if (streamSnapshot.connectionState == ConnectionState.waiting) {
+      //       return Center(
+      //         child: CircularProgressIndicator(),
+      //       );
+      //     }
+      //     final documents = streamSnapshot.data.docs;
+      //     return ListView.builder(
+      //       itemCount: documents.length,
+      //       itemBuilder: (ctx, index) => Container(
+      //         padding: EdgeInsets.all(8),
+      //         child: Text(documents[index]['text']),
+      //       ),
+      //     );
+      //   },
+      // ),
+      // floatingActionButton: FloatingActionButton(
+      //   child: Icon(Icons.add),
+      //   onPressed: () {
+      //     FirebaseFirestore.instance
+      //         .collection('chats/vr7grvLiKtVIsPWvUsYB/messages')
+      //         .add({'text': 'This was added by clicking the button'});
+      //   },
+      // ),
     );
   }
 }
---
+++ chat_app/lib/widgets/chat/messages.dart
@@ -0,0 +1,34 @@
+// import 'dart:html';
+
+
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+import '../../widgets/chat/message_bubble.dart';
+
+class Messages extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder(
+      stream: FirebaseFirestore.instance
+          .collection('chat')
+          .orderBy('createdAt', descending: true)
+          .snapshots(),
+      builder: (ctx, chatSnapshot) {
+        if (chatSnapshot.connectionState == ConnectionState.waiting) {
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+        final chatDocs = chatSnapshot.data.documents;
+        return ListView.builder(
+          reverse: true,
+          itemCount: chatDocs.length,
+          itemBuilder: (ctx, index) => MessageBubble(
+            chatDocs[index]['text'],
+          ),
+        );
+      },
+    );
+  }
+}
